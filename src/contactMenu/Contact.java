66970de342767fe5451b698638aa1fbdfee81058
RamanMyreign <raman.shukla_cs18@gla.ac.in>
2020-02-23 15:28:53 +0530

    implements comparable for sorting contacts

---
+++ src/contactMenu/Contact.java
@@ -0,0 +1,26 @@
+package contactMenu;
+
+import java.util.LinkedList;
+
+public class Contact implements Comparable<Contact> {
+    private LinkedList<Integer> mobileNo;
+    private String name;
+    private String lastName;
+    private Contact contact;
+    private String email;
+
+    public Contact(String name, String lastName, String email, LinkedList<Integer> mobileNo) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.mobileNo = mobileNo;
+
+    }
+
+
+    public int compareTo(Contact contact) {
+        return name.compareTo(contact.name);
+    }
+
+
+}
