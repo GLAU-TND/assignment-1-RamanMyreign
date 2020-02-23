58f2fb8aae14fb58dc464b2088f0659c9d710726
RamanMyreign <raman.shukla_cs18@gla.ac.in>
2020-02-23 15:40:06 +0530

    addContact function is created.This function have two helper functions -addMobile and addEmail

---
+++ src/contactMenu/ContactMenuMethods.java
@@ -0,0 +1,58 @@
+package contactMenu;
+
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class ContactMenuMethods {
+    private LinkedList<Integer> mobileNo;
+    private Contact contact;
+    private String email;
+    private LinkedList<Contact> list = new LinkedList<>();
+
+
+    public void addContact() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have chosen to add a new contact: \n" +
+                "Please enter the name of the Person");
+        System.out.print("First Name:");
+        String name = scan.next();
+        System.out.print("Last Name: ");
+        String lastName = scan.next();
+        System.out.print("Enter Mobile No:");
+        mobileNo = new LinkedList<>();
+        mobileNo.add(scan.nextInt());
+        addMobile();
+        addemail();
+        Contact contact = new Contact(name, lastName, email, mobileNo);
+        list.add(contact);
+        Collections.sort(list);
+
+    }
+
+    private void addMobile() {
+        System.out.print("do you want to add another contact? (y/n) : ");
+        Scanner scan = new Scanner(System.in);
+        String add = scan.next();
+        if (add.equals("y")) {
+            mobileNo.add(scan.nextInt());
+            addMobile();
+        } else {
+        }
+    }
+
+    private void addemail() {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Do you want to add email address? (y/n)");
+        String add = scan.next();
+        if (add.equals("y")) {
+            email = scan.next();
+
+        } else {
+            email = "---";
+        }
+
+    }
+}
